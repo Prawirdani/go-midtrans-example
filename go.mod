12a01733c632b3bcccb6b98d90e950ac17a87d9b
Prawirdani <prawirdani@gmail.com>
2024-09-23 15:55:31 +0700

    env vars

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/prawirdani/go-midtrans-example
 
 go 1.22.5
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -4,18 +4,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/prawirdani/go-midtrans-example/pkg/http/mux"
 	"github.com/prawirdani/go-midtrans-example/pkg/http/response"
 )
 
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file", err)
+	}
+}
+
 func main() {
+	PORT := os.Getenv("PORT")
+
 	mux := mux.New()
 
 	mux.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		_ = response.Send(w, response.WithMessage("Hello, World!"))
 	})
 
-	fmt.Println("Server started at :8080")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	fmt.Println("Server running on port", PORT)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", PORT), mux))
 }
